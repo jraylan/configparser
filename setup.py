834bad0cbb4026be44f5231f7256b952b764d94b
Jason R. Coombs <jaraco@jaraco.com>
2019-01-28 19:38:06 -0500

    Rely on setuptools 40.7 to support declarative config for src pkg layout

--- setup.py
+++ setup.py
@@ -4,6 +4,4 @@ import setuptools
 setuptools.setup(
     # enable `import configparser` for Python 2
     py_modules=['configparser'] if sys.version_info < (3,) else [],
-    packages=setuptools.find_packages(where='src'),
-    package_dir={'': 'src'},
 )
